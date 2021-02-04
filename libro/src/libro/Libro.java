6382b68ea9d4d75f028400592a5fc2ba71269ff5
Diego <pol_rubiano@yahoo.es>
2021-02-04 19:06:58 +0100

    añadida clase Libro

--- libro/src/libro/Libro.java
+++ libro/src/libro/Libro.java
@@ -1,5 +1,50 @@
 package libro;
 
 public class Libro {
+	
+	private String titulo;
+	private String autor;
+	private boolean disponible;
 
+	Libro (){
+		this.autor = null;
+		this.titulo = null;
+		this.disponible = true;
+	}
+	
+	Libro(String titulo, String autor, boolean disponible){
+		this.autor = autor;
+		this.titulo = titulo;
+		this.disponible = disponible;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public boolean isDisponible() {
+		return disponible;
+	}
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}	
+	public String toString() {
+		return "titulo: " + this.titulo + "\nautor: " + this.autor; 
+	}
+	
+	public void prestamo() {
+		this.disponible = false;
+	}
+	
+	public void devolucion() {
+		this.disponible = true;		
+	}
 }
